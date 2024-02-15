05cfb92909821e79e64eb452b7957591951bbba4
M. J. Fromberger <fromberger@tailscale.com>
2024-02-14 16:56:17 -0800

    all: add copyright notices to source

--- squibble.go
+++ squibble.go
@@ -1,3 +1,6 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
 // Package squibble provides a schema migration assistant for SQLite databases.
 //
 // # Overview
